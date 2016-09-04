99b34146899bf9f6e53056dfdd2e0bdfae172187
Volodymyr Medvid <vmedvid@riseup.net>
2016-09-05 00:27:22 +0300

    Add copy template script

---
+++ copy_template.bat
@@ -0,0 +1,1 @@
+bash copy_template.sh %*
---
+++ copy_template.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+SRC=Ex00-Template
+DEST=$1
+echo D=$DEST
+
+if [[ -z ${DEST} ]]; then
+  echo "Usage: ${0} Ex01_MyProject"
+  exit 1
+fi
+
+cp -rv ${SRC} ${DEST}
+rm -rf ${DEST}/{IAR,MDK}/*-{Debug,Release}
+/usr/bin/find ${DEST} -type f | while read FILE ; do
+    target=$(echo ${FILE} |sed -e "s/${SRC}/${DEST}/")
+    mv ${FILE} ${target}
+    sed -e "s/${SRC}/${DEST}/g" -i ${target}
+done
